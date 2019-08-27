0e176fb6e7466c155137b5299e0f7a1aabc8b4c1
isdrobo2019 <miki.nakahara@isd.co.jp>
2019-08-27 18:48:04 +0900

    色検知ジグザグ

---
+++ proj/app/Straight.cpp
@@ -0,0 +1,37 @@
+#include "Straight.h"
+#include "stdlib.h"
+Straight::Straight():
+  leftWheel(PORT_C), rightWheel(PORT_B),
+  colorSensor(PORT_2) {
+  }
+
+void Straight::init() {
+  init_f("083_sample");
+}
+
+void Straight::terminate() {
+  msg_f("Stopped.", 1);
+  leftWheel.stop();
+  rightWheel.stop();
+}
+
+void Straight::run(int target,int flg) {
+
+	int pwm = 15;
+
+	if(pwm < labs(turn)){
+		if(turn > 0 ){
+			turn = pwm;
+		} else {
+			turn = -pwm;
+		}
+	}
+
+	int turn = leftWheel.getCount() - rightWheel.getCount();
+	turn /= abs(turn);
+
+	leftWheel.setPWM(pwm - turn);
+	rightWheel.setPWM(pwm + turn);
+
+}
+
